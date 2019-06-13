af9af0099aa3a7f5fefd153a1e80ad4d51ec945e
Andy Lam <>
2019-06-13 18:15:55 +0800

    Change display score code, not working

--- model.js
+++ model.js
@@ -1,28 +1,42 @@
 // REQUIREMENTS
 
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-const fs = require('fs');
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const fs = require("fs");
 
-const adapter = new FileSync('db.json');
+const adapter = new FileSync("db.json");
 const db = low(adapter);
 
 // HELPER GETTERS
 
 function getUser(userId) {
-  return db.get('users').find({ id: userId }).value();
+  return db
+    .get("users")
+    .find({ id: userId })
+    .value();
 }
 
 function getSuperUser(userId) {
-  return db.get('superusers').find({ id: userId }).value();
+  return db
+    .get("superusers")
+    .find({ id: userId })
+    .value();
 }
 
 function getHouse(houseId) {
-  return db.get('houses').find({ id: houseId }).value();
+  return db
+    .get("houses")
+    .find({ id: houseId })
+    .value();
 }
 
 function getOg(houseId, ogId) {
-  return db.get('houses').find({ id: houseId }).get('ogs').find({ id: ogId }).value();
+  return db
+    .get("houses")
+    .find({ id: houseId })
+    .get("ogs")
+    .find({ id: ogId })
+    .value();
 }
 
 /**
@@ -31,41 +45,45 @@ function getOg(houseId, ogId) {
  * @param {string} userId - id of the user doing reset
  */
 
-module.exports.reset = function (userId) {
+module.exports.reset = function(userId) {
   return new Promise((resolve, reject) => {
     const user = getSuperUser(userId);
-    
+
     if (user === undefined) {
-      reject('You are not a superuser!');
+      reject("You are not a superuser!");
       return;
     }
 
-    fs.unlinkSync("data.csv", err => {})
-    const json = db.getState().houses
+    fs.unlinkSync("data.csv", err => {});
+    const json = db.getState().houses;
 
-    let fields = Object.keys(json[0])
-    let replacer = function (key, value) { return value === null ? '' : value }
-    let csv = json.map(function (row) {
-      return fields.map(function (fieldName) {
-        return JSON.stringify(row[fieldName], replacer)
-      }).join(',')
-    })
-    csv.unshift(fields.join(',')) // add header column
-    csv.join('\r\n')
+    let fields = Object.keys(json[0]);
+    let replacer = function(key, value) {
+      return value === null ? "" : value;
+    };
+    let csv = json.map(function(row) {
+      return fields
+        .map(function(fieldName) {
+          return JSON.stringify(row[fieldName], replacer);
+        })
+        .join(",");
+    });
+    csv.unshift(fields.join(",")); // add header column
+    csv.join("\r\n");
     fs.writeFile("data.csv", csv, function(err) {});
 
-    db.get('houses')
+    db.get("houses")
       .map(house => {
         house.score = 0;
         house.ogs.map(og => {
           og.score = 0;
-        })
+        });
       })
       .write();
 
     resolve("data.csv");
   });
-}
+};
 
 /**
  * Add score to a house.
@@ -76,37 +94,33 @@ module.exports.reset = function (userId) {
  * @returns {object} - Updated house object.
  */
 
-module.exports.addHouseScore = function (houseId, score, userId) {
-
+module.exports.addHouseScore = function(houseId, score, userId) {
   return new Promise((resolve, reject) => {
-
     const user = getUser(userId);
     const house = getHouse(houseId);
 
     if (user === undefined) {
-      reject('Error adding score: invalid user');
+      reject("Error adding score: invalid user");
       return;
     }
 
     if (house === undefined) {
-      reject('Error adding score: invalid house id');
+      reject("Error adding score: invalid house id");
       return;
     }
 
     if (!Number.isInteger(score)) {
-      reject('Error adding score: score not an integer');
+      reject("Error adding score: score not an integer");
       return;
     }
 
-    db.get('houses')
+    db.get("houses")
       .find({ id: houseId })
-      .set('score', house.score + score)
+      .set("score", house.score + score)
       .write();
 
     resolve({ house });
-
   });
-
 };
 
 /**
@@ -119,45 +133,41 @@ module.exports.addHouseScore = function (houseId, score, userId) {
  * @returns {object} - Updated house and og object.
  */
 
-module.exports.addOgScore = function (houseId, ogId, score, userId) {
-
+module.exports.addOgScore = function(houseId, ogId, score, userId) {
   return new Promise((resolve, reject) => {
-
     const user = getUser(userId);
     const house = getHouse(houseId);
     const og = getOg(houseId, ogId);
 
     if (user === undefined) {
-      reject('Error adding score: invalid user');
+      reject("Error adding score: invalid user");
       return;
     }
 
     if (house === undefined) {
-      reject('Error adding score: invalid house id');
+      reject("Error adding score: invalid house id");
       return;
     }
 
     if (og === undefined) {
-      reject('Error adding score: invalid og id');
+      reject("Error adding score: invalid og id");
       return;
     }
 
     if (!Number.isInteger(score)) {
-      reject('Error adding score: score not an integer');
+      reject("Error adding score: score not an integer");
       return;
     }
 
-    db.get('houses')
+    db.get("houses")
       .find({ id: houseId })
-      .get('ogs')
+      .get("ogs")
       .find({ id: ogId })
-      .set('score', og.score + score)
+      .set("score", og.score + score)
       .write();
 
     resolve({ house, og });
-
   });
-
 };
 
 /**
@@ -167,28 +177,25 @@ module.exports.addOgScore = function (houseId, ogId, score, userId) {
  * @param {integer} userId - ID of the user invoking this request.
  */
 
-module.exports.addUser = function (targetId, userId) {
-
+module.exports.addUser = function(targetId, userId) {
   return new Promise((resolve, reject) => {
     if (Number.isNaN(targetId)) {
-      reject('Error: invalid userId');
+      reject("Error: invalid userId");
       return;
     }
     const user = getUser(userId);
 
     if (user === undefined) {
-      reject('Error adding user: invalid user');
+      reject("Error adding user: invalid user");
       return;
     }
 
-    db.get('users')
+    db.get("users")
       .push({ id: targetId })
       .write();
 
     resolve();
-
   });
-
 };
 
 /**
@@ -197,9 +204,9 @@ module.exports.addUser = function (targetId, userId) {
  * @returns {array} - Array of objects, each is a house.
  */
 
-module.exports.getHousesModel = function () {
+getHousesModel = () => {
   return new Promise((resolve, reject) => {
-    resolve(db.get('houses').value());
+    resolve(db.get("houses").value());
   });
 };
 
@@ -208,56 +215,89 @@ module.exports.addOg = function(houseId, ogId, ogName, userId) {
     const user = getUser(userId);
 
     if (user === undefined) {
-      reject('Error adding user: invalid user');
+      reject("Error adding user: invalid user");
       return;
     }
 
-    db.get('houses').find({ id: houseId }).get('ogs').push({
-      "id": ogId,
-      "name": ogName,
-      "score": 0
-    }).write();
+    db.get("houses")
+      .find({ id: houseId })
+      .get("ogs")
+      .push({
+        id: ogId,
+        name: ogName,
+        score: 0
+      })
+      .write();
 
     resolve();
   });
-}
+};
 
 module.exports.addHouse = function(houseId, houseName, userId) {
   return new Promise((resolve, reject) => {
-
     const user = getUser(userId);
 
     if (user !== 193836494 || user !== 346012334) {
-      reject('Error adding user: invalid user');
+      reject("Error adding user: invalid user");
       return;
     }
 
-    db.get('houses').push({
-      "id": houseId,
-      "name": houseName,
-      "score": 0,
-      "ogs": []
-    }).write();
+    db.get("houses")
+      .push({
+        id: houseId,
+        name: houseName,
+        score: 0,
+        ogs: []
+      })
+      .write();
 
     resolve();
   });
-}
+};
 
 module.exports.removeOg = function(houseId, ogId, userId) {
   return new Promise((resolve, reject) => {
-
     const user = getUser(userId);
 
     if (user === undefined) {
-      reject('You are not an admin!');
+      reject("You are not an admin!");
       return;
     }
-    
-    db.get('houses')
-      .find({id : houseId})
-      .get('ogs')
-      .remove({id : ogId});
-    
-    resolve('Removed successfully');
+
+    db.get("houses")
+      .find({ id: houseId })
+      .get("ogs")
+      .remove({ id: ogId });
+
+    resolve("Removed successfully");
   });
-}
+};
+
+module.exports.ds = userId => {
+  return new Promise(async (resolve, reject) => {
+    const user = getUser(userId);
+
+    if (user === undefined) {
+      reject("You are not an admin!");
+      return;
+    }
+    const housesModel = await getHousesModel();
+    if (!housesModel) {
+      reject("Nothing to diplay");
+    }
+    const houseMessage = housesModel.map(house => {
+      const totalScore = house.ogs.reduce(
+        (score, og) => score + og.score,
+        house.score
+      );
+      return house.ogs.reduce((accumulator, og) => {
+        return `${accumulator}\n${og.name} (${og.id}) - \`${og.score}\``;
+      }, `*${house.name} (${house.id}) - ${totalScore} = ${totalScore - house.score} + ${house.score}*`);
+    });
+    const message = houseMessage.reduce(
+      (accumulator, mess) => `${accumulator}\n\n${mess}`
+    );
+    resolve(message);
+    return;
+  });
+};
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 // REQUIREMENTS
-const dotenv = require('dotenv');
-const express = require('express');
-const Telegraf = require('telegraf');
-const Model = require('./model');
-const http = require('http');
+const dotenv = require("dotenv");
+const express = require("express");
+const Telegraf = require("telegraf");
+const Model = require("./model");
+const http = require("http");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
+  console.log(`Our app is running on port ${PORT}`);
 });
 // DOTENV SETUP
 
@@ -18,7 +18,9 @@ dotenv.config();
 // BOT SETUP
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-const website = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendDocument`;
+const website = `https://api.telegram.org/bot${
+  process.env.BOT_TOKEN
+}/sendDocument`;
 
 bot.telegram.getMe().then(botInfo => {
   bot.options.username = botInfo.username;
@@ -27,66 +29,78 @@ bot.telegram.getMe().then(botInfo => {
 // START COMMAND
 
 bot.start(ctx => {
-  let message = '';
-  message += 'Hello! I can help you keep track of scores. You can control me by sending me these commands.\n';
-  message += '\n';
-  message += '/addhousescore - add house score\n';
-  message += '/addogscore - add og score\n';
-  message += '/adduser - add user\n';
-  message += '/help - show detailed help\n';
-  message += '/displayscore - display score\n';
-  message += '/who - get user ID\n';
+  let message = "";
+  message +=
+    "Hello! I can help you keep track of scores. You can control me by sending me these commands.\n";
+  message += "\n";
+  message += "/addhousescore - add house score\n";
+  message += "/addogscore - add og score\n";
+  message += "/adduser - add user\n";
+  message += "/help - show detailed help\n";
+  message += "/displayscore - display score\n";
+  message += "/who - get user ID\n";
   return ctx.reply(message);
 });
 
 // HELP COMMAND
 
 bot.help(ctx => {
-  let message = '';
-  message += 'This is a telegram bot created to keep track of scores for Computing FOP! ';
-  message += 'Only admins can make changes to scores - others can only view scores.\n';
-  message += '\n';
-  message += '*Commands:* \n';
-  message += 'Add a new house. \n';
-  message += '/addhouse (houseId) (houseName). \n'
-  message += '\n';
-  message += 'Add a new og. \n';
-  message += '/addog (houseId) (ogId) (ogName). \n'
-  message += '\n';
-  message += 'Add (score) to (houseId).\n';
-  message += '/addhousescore (houseId) (score)\n';
-  message += '\n';
-  message += 'Add (score) to (ogId) from (houseId).\n';
-  message += '/addogscore (houseId) (ogId) (score)\n';
-  message += '\n';
-  message += 'Add an admin (you must be an admin) (userId).\n';
-  message += '/addadmin (userId)\n';
-  message += '\n';
-  message += 'Display score.\n';
-  message += '/displayscore\n';
-  message += '\n';
-  message += 'Get user ID.\n';
-  message += '/who\n';
-  message += '\n';
-  message += 'Obtain a .csv file of current points and reset all points to zero\n'
-  message += '/reset';
-  return ctx.telegram.sendMessage(ctx.chat.id, message, {parse_mode: 'Markdown',
-    reply_to_message_id: ctx.message.message_id});
+  let message = "";
+  message +=
+    "This is a telegram bot created to keep track of scores for Computing FOP! ";
+  message +=
+    "Only admins can make changes to scores - others can only view scores.\n";
+  message += "\n";
+  message += "*Commands:* \n";
+  message += "Add a new house. \n";
+  message += "/addhouse (houseId) (houseName). \n";
+  message += "\n";
+  message += "Add a new og. \n";
+  message += "/addog (houseId) (ogId) (ogName). \n";
+  message += "\n";
+  message += "Add (score) to (houseId).\n";
+  message += "/addhousescore (houseId) (score)\n";
+  message += "\n";
+  message += "Add (score) to (ogId) from (houseId).\n";
+  message += "/addogscore (houseId) (ogId) (score)\n";
+  message += "\n";
+  message += "Add an admin (you must be an admin) (userId).\n";
+  message += "/addadmin (userId)\n";
+  message += "\n";
+  message += "Display score.\n";
+  message += "/displayscore\n";
+  message += "\n";
+  message += "Get user ID.\n";
+  message += "/who\n";
+  message += "\n";
+  message +=
+    "Obtain a .csv file of current points and reset all points to zero\n";
+  message += "/reset";
+  return ctx.telegram.sendMessage(ctx.chat.id, message, {
+    parse_mode: "Markdown",
+    reply_to_message_id: ctx.message.message_id
+  });
 });
 
 // addhousescore COMMAND
 
-bot.command(['addhousescore', 't'], async ctx => {
-  const args = ctx.message.text.split(' ');
+bot.command(["addhousescore", "t"], async ctx => {
+  const args = ctx.message.text.split(" ");
   const houseId = args[1];
   const score = Number(args[2]);
   const userId = ctx.from.id;
- 
+
   try {
     const res = await Model.addHouseScore(houseId, score, userId);
-    const newScore = res.house.ogs.reduce((accumulator, og) => accumulator + og.score, res.house.score);
+    const newScore = res.house.ogs.reduce(
+      (accumulator, og) => accumulator + og.score,
+      res.house.score
+    );
     const message = `*${res.house.name}* has *${newScore}* points.`;
-    return ctx.telegram.sendMessage(ctx.chat.id, message, { parse_mode: 'Markdown', reply_to_message_id: ctx.message.message_id });
+    return ctx.telegram.sendMessage(ctx.chat.id, message, {
+      parse_mode: "Markdown",
+      reply_to_message_id: ctx.message.message_id
+    });
   } catch (err) {
     return ctx.reply(err);
   }
@@ -94,17 +108,22 @@ bot.command(['addhousescore', 't'], async ctx => {
 
 // ADDOGSCORE COMMAND
 
-bot.command(['addogscore', 's'], async ctx => {
-  const args = ctx.message.text.split(' ');
+bot.command(["addogscore", "s"], async ctx => {
+  const args = ctx.message.text.split(" ");
   const houseId = args[1];
   const ogId = args[2];
   const score = Number(args[3]);
   const userId = ctx.from.id;
-  
+
   try {
     const res = await Model.addOgScore(houseId, ogId, score, userId);
-    const message = `*${res.og.name}* from *${res.house.name}* has *${res.og.score} points*`;
-    return ctx.telegram.sendMessage(ctx.chat.id, message, { parse_mode: 'Markdown', reply_to_message_id: ctx.message.message_id });
+    const message = `*${res.og.name}* from *${res.house.name}* has *${
+      res.og.score
+    } points*`;
+    return ctx.telegram.sendMessage(ctx.chat.id, message, {
+      parse_mode: "Markdown",
+      reply_to_message_id: ctx.message.message_id
+    });
   } catch (err) {
     return ctx.reply(err);
   }
@@ -112,11 +131,11 @@ bot.command(['addogscore', 's'], async ctx => {
 
 // ADDUSER COMMAND
 
-bot.command(['addadmin', 'u'], async ctx => {
-  const args = ctx.message.text.split(' ');
+bot.command(["addadmin", "u"], async ctx => {
+  const args = ctx.message.text.split(" ");
   const targetId = Number(args[1]);
   const userId = ctx.from.id;
-  
+
   try {
     const newScore = await Model.addUser(targetId, userId);
     const message = `User ${targetId} has been added`;
@@ -128,44 +147,55 @@ bot.command(['addadmin', 'u'], async ctx => {
 
 // DISPLAYSCORE COMMAND
 
-async function ds() {
-  const housesModel = await Model.getHousesModel();
-  const houseMessage = housesModel.map(house => {
-    const totalScore = house.ogs.reduce((score, og) => score + og.score, house.score);
-    return house.ogs.reduce((accumulator, og) => {
-      return `${accumulator}\n${og.name} (${og.id}) - \`${og.score}\``;
-    }, `*${house.name} (${house.id}) - ${totalScore} = ${totalScore - house.score} + ${house.score}*`);
-  });
-  
-  const message = houseMessage.reduce((accumulator, mess) => `${accumulator}\n\n${mess}`);
-  return message;
-}
+// async function ds() {
+// const housesModel = await Model.getHousesModel();
+// const houseMessage = housesModel.map(house => {
+// const totalScore = house.ogs.reduce(
+// (score, og) => score + og.score,
+// house.score
+// );
+// return house.ogs.reduce((accumulator, og) => {
+// return `${accumulator}\n${og.name} (${og.id}) - \`${og.score}\``;
+// }, `*${house.name} (${house.id}) - ${totalScore} = ${totalScore - house.score} + ${house.score}*`);
+// });
 
-bot.command(['displayscore', 'ds'], async ctx => {
-  const message = await ds();
-  return ctx.replyWithMarkdown(message);
+// const message = houseMessage.reduce(
+// (accumulator, mess) => `${accumulator}\n\n${mess}`
+// );
+// return message;
+// }
+
+bot.command(["displayscore", "ds"], async ctx => {
+  const userId = ctx.from.id;
+  Model.ds(userId)
+    .then(res => ctx.replyWithMarkdown(res))
+    .catch(err => ctx.replyWithMarkdown(err));
 });
 
 // WHO COMMAND
 
-bot.command('who', async ctx => {
-  return ctx.replyWithMarkdown(`${ctx.from.first_name} your ID is \`${ctx.from.id}\``);
+bot.command("who", async ctx => {
+  return ctx.replyWithMarkdown(
+    `${ctx.from.first_name} your ID is \`${ctx.from.id}\``
+  );
 });
 
-bot.command('reset', async ctx => {
+bot.command("reset", async ctx => {
   const userId = ctx.from.id;
-  const message = await ds();
-  
-  Model.reset(userId).then(res => {
-    ctx.replyWithMarkdown(message);
-    return ctx.replyWithDocument({source: document});
-  }).catch(err => {
-    ctx.reply(err);
-  });
+  const message = Model.ds(userId).catch(err => ctx.replyWithMarkdown(err));
+
+  Model.reset(userId)
+    .then(res => {
+      ctx.replyWithMarkdown(message);
+      return ctx.replyWithDocument({ source: document });
+    })
+    .catch(err => {
+      ctx.reply(err);
+    });
 });
 
-bot.command('addhouse', async ctx => {
-  const args = ctx.message.text.split(' ');
+bot.command("addhouse", async ctx => {
+  const args = ctx.message.text.split(" ");
   const houseId = args[1];
   const houseName = args[2];
   const userId = ctx.from.id;
@@ -175,12 +205,12 @@ bot.command('addhouse', async ctx => {
     const message = `${houseName} has been added. Please add OGs!`;
     return ctx.reply(message);
   } catch (err) {
-    return ctx.reply('Unknown error');
+    return ctx.reply("Unknown error");
   }
-})
+});
 
-bot.command('addog', async ctx => {
-  const args = ctx.message.text.split(' ');
+bot.command("addog", async ctx => {
+  const args = ctx.message.text.split(" ");
   const houseId = args[1];
   const ogId = args[2];
   const ogName = args[3];
@@ -193,10 +223,10 @@ bot.command('addog', async ctx => {
   } catch (err) {
     return ctx.reply(err);
   }
-})
+});
 
-bot.command('removeog', async ctx => {
-  const args = ctx.message.text.split(' ');
+bot.command("removeog", async ctx => {
+  const args = ctx.message.text.split(" ");
   const houseId = args[1];
   const ogId = args[2];
   const userId = ctx.from.id;
@@ -208,7 +238,7 @@ bot.command('removeog', async ctx => {
   } catch (err) {
     return ctx.reply(err);
   }
-})
+});
 // BOT POLL
 
-bot.startPolling()
+bot.startPolling();
